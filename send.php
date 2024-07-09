90f76bb278f54fe2e0a3858f4f1ce105c27eac7b
owo233 <ver@vip.qq.com>
2024-07-09 12:33:34 +0800

    rdelivery: Precision Output

--- send.php
+++ send.php
@@ -5,10 +5,9 @@ require_once 'curl.php';
 require_once 'util.php';
 
 header('Content-Type: application/json');
-
 $param = getParameters(); // 获取GET参数
 if ($param === false) {
-    exit(jsonExit(400, 'Bad Request')); // 参数错误
+    exit(jsonExit(400, 'Bad Request'));
 }
 $key = generateAESKey(); // 生成AES密钥
 $data = generateJsonString($param['version'], $param['uin']); // 生成JSON字符串
@@ -25,9 +24,17 @@ $post = [
     ]
 ]; // 生成POST数据
 $result = postJsonWithCurl('https://rdelivery.qq.com/v3/config/batchpull', $post); // 发送POST请求
-$text = getCipherText($result);
+$text = getCipherText($result); // 解析JSON并返回cipher_text
 if ($text === false) {
-    exit(jsonExit(500, 'Internal Server Error'));
+    exit(jsonExit(500, 'cipher_text does not exist'));
 }
-$decode = aesDecrypt(base64_decode($text), $key); // 解密字符串
-exit(gzdecode($decode)); // 解压字符串并返回
+$data = json_decode(gzdecode(aesDecrypt(base64_decode($text), $key)), true); // 解密并解析返回数据
+if (json_last_error() !== JSON_ERROR_NONE) {
+    exit(jsonExit(501, 'cannot parse returned data'));
+}
+if (!isset($data['configs'])) {
+    exit(jsonExit(502, '该QQ目前不存在更新推送或已经是最新版本'));
+}
+$value_content = json_decode($data['configs'][0]['value'], true);
+$config_value_content = json_decode($value_content['config_value'], true);
+exit(json_encode($config_value_content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
